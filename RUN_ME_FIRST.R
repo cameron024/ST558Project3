5abd08b984338bbf9d6720cf3194f782a791dd0a
cameron024 <38161651+cameron024@users.noreply.github.com>
2019-07-30 22:55:09 -0400

    Update RUN_ME_FIRST.R

--- RUN_ME_FIRST.R
+++ RUN_ME_FIRST.R
@@ -1,4 +1,6 @@
+# Download the pbp-2018.csv dataset
+
 # Read in the nfl data
-nfl <- read.csv("/Users/Cameron_E/ShinyApps/Project 3/pbp-2018.csv") 
+nfl <- read.csv("path/to/your/directory/containing/pbp-2018.csv") 
 nfl <- nfl %>% select(OffenseTeam, DefenseTeam, Down, ToGo, Yards, Formation, 
                         PlayType, PassType, IsIncomplete, RushDirection, IsFumble)
